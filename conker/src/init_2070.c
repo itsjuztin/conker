616b308e102f6b1183e17b6fc27f8a338e809e78
itsjuztin <imthatkindofperson@gmail.com>
2026-09-05 19:18:30 -0400

    Match func_10002088 in init_2070.c (variadic printf wrapper)

--- conker/src/init_2070.c
+++ conker/src/init_2070.c
@@ -1,4 +1,5 @@
 #include <ultra64.h>
+#include <stdarg.h>
 
 #include "functions.h"
 #include "variables.h"
@@ -8,12 +9,12 @@ s32 func_10002070(s32 arg0, s32 arg1, s32 arg2) {
     return 1;
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/init_2070/func_10002088.s")
-// NOT MATCHING: stack is incorrect
-// void func_10002088(s32 arg0, s32 arg1, s32 arg2, s32 arg3) {
-//     D_80035500 = 0;
-//     func_100020D0(func_10002070, 0, arg1, &arg1);
-// }
+void func_10002088(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    D_80035500 = 0;
+    func_100020D0(func_10002070, 0, fmt, args);
+}
 
 // this is a beast:
 #pragma GLOBAL_ASM("asm/nonmatchings/init_2070/func_100020D0.s")
